5c2ac4a8dc9049f04167941a9daf878484b6ee20
Mark Lewis <thnkloud9@gmail.com>
2015-12-07 17:40:34 +0100

    adding checksum to newscoop

--- newscoop.rb
+++ newscoop.rb
@@ -5,7 +5,8 @@
 class Newscoop < Formula
   homepage "https://github.com/sourcefabric/Newscoop"
   url "https://github.com/sourcefabric/Newscoop/archive/4.4.6.tar.gz"
-  sha1 "3d150201408349c0ec9618ef5e093a2398b86b6f6b81fbf2692fb253762f2c69"
+  #sha1 "3d150201408349c0ec9618ef5e093a2398b86b6f6b81fbf2692fb253762f2c69"
+  sha1 "da4396410b03f1174b9e5372da61adb564f9bd0b"
 
   depends_on "php56"
   depends_on "php56-pdo-pgsql"
